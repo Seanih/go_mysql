a9f8736468faa050193f7ed2e85b42475fd5d1bc
Sean Anih <sean.anih@gmail.com>
2023-03-22 01:18:30 -0400

    initial commit; established connection to mysql db

--- main.go
+++ main.go
@@ -7,27 +7,39 @@ import (
 	"os"
 
 	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var db *sql.DB
 
 func main() {
-	// Capture connection properties
+	dbConnection()
+}
+
+func dbConnection() {
+	// load env variables
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	// Capture connection properties.
 	cfg := mysql.Config{
-		User:   os.Getenv("DBUSER"),
-		Passwd: os.Getenv("DBPASS"),
+		User:   os.Getenv("DB_USERNAME"),
+		Passwd: os.Getenv("DB_PASSWORD"),
 		Net:    "tcp",
 		Addr:   "127.0.0.1:3306",
 		DBName: "recordings",
 	}
 
-	// Get a database handle
-	var err error
+	// connect to db
 	db, err = sql.Open("mysql", cfg.FormatDSN())
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// verify if db connection is live; reconnect if not
 	pingErr := db.Ping()
 	if pingErr != nil {
 		log.Fatal(pingErr)
